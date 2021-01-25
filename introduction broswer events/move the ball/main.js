2ddefc2fa049594bbde68d7b6787238497e6c12b
gil6464 <77244365+gil6464@users.noreply.github.com>
2021-01-25 19:26:55 +0200

    changed the ball from pic to div
    and started the script writing

---
+++ introduction broswer events/move the ball/main.js
@@ -0,0 +1,9 @@
+// document.addEventListener("DOMCotentLoaded", startMatch);
+
+const ball = document.querySelector("#ball");
+
+// function startMatch() {
+//      let fieldCoords = this.getBoundingClientRect();
+//      const div = document.querySelector("#background");
+
+//      div.addEventListener("click";
