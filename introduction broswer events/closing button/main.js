8a8d4cce16554d391169abfbb16f1035137c6f47
gil6464 <77244365+gil6464@users.noreply.github.com>
2021-01-25 23:43:44 +0200

    solve with "for of loop"
    made a for of loop, that make button for each of the paragraph, and when that button get clicked as the child of his paragraph, only this paragraph is deleted

--- introduction broswer events/closing button/main.js
+++ introduction broswer events/closing button/main.js
@@ -1,17 +1,16 @@
 document.addEventListener("DOMContentLoaded", start);
 
 function start() {
-     const first = document.querySelector("#first");
-     const second = document.querySelector("#second");
-     const third = document.querySelector("#third");
+     let paragraph = document.querySelectorAll(".para");
+   
+    for (let para of paragraph) {
+      para.insertAdjacentHTML("afterbegin", '<button class="delete-button">x</button>');
+    
+      para.firstChild.addEventListener("click", deleteMe);
 
-     first.insertAdjacentHTML("afterbegin", '<button class="delete-button">x</button>');
-     second.insertAdjacentHTML("afterbegin", '<button class="delete-button">x</button>');
-     third.insertAdjacentHTML("afterbegin", '<button class="delete-button">x</button>');
-
-     first.addEventListener("click", deletePara);
-     function deletePara() {
-     }
-     let deleteButton = document.querySelector("delete-button");
-
-}
+      function deleteMe() { 
+          para.remove();
+      }
+    };
+  
+};
