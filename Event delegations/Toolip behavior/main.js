4b735bd050bacc88abec18e558318161858cd53c
gil6464 <77244365+gil6464@users.noreply.github.com>
2021-01-26 22:45:10 +0200

    tootlip done
    make the shadow disapear by the remove function, and also located him as requsted. add in the html page "go back" heading, in to bottom of the page.

--- Event delegations/Toolip behavior/main.js
+++ Event delegations/Toolip behavior/main.js
@@ -1,18 +1,31 @@
-document.addEventListener("DOMContentLoaded", () =>{
+document.addEventListener("DOMContentLoaded", () => {
 
-     const button = document.querySelector("#one")
      document.addEventListener("mouseover",(event) => {
-          let target = event.target;
+      let target = event.target;
           
-          let tooltipHtml = target.dataset.tooltip;
-          if (!tooltipHtml) return;
-          console.log("gil");
+      let tooltipHtml = target.dataset.tooltip;
+        if (!tooltipHtml) return;
           
+      let tooltipElem = document.createElement('div');
+       tooltipElem.className = 'tooltip';
+       tooltipElem.innerHTML = tooltipHtml;
+       document.body.append(tooltipElem);
           
-          let tooltipElem = document.createElement('div');
-           tooltipElem.className = 'tooltip';
-           tooltipElem.innerHTML = tooltipHtml;
-           document.body.append(tooltipElem)
+      document.addEventListener("mouseover", ()=> { // makes the data window disapear when mouse is not over
+        if (tooltipElem) {
+         tooltipElem.remove();
+        }
+      });
 
-           })
+      let coordinates = target.getBoundingClientRect();
+      let left = coordinates.left + (target.offsetWidth - tooltipElem.offsetWidth);
+       if (left < 0) left = 0; 
+               
+      let top = coordinates.top - tooltipElem.offsetHeight - 5;
+       if (top < 0) { 
+          top = coordinates.top + target.offsetHeight + 5;
+      } // adjust the shadow to apear near the buttom as requsted
+       tooltipElem.style.left = left + 'px';
+       tooltipElem.style.top = top + 'px';
+     });
 });
